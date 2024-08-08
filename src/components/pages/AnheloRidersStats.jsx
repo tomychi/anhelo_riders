beef32b8bf6e6ca2fdb55548980903268600f1cb
Tobías <tobiasazcurrabi@gmail.com>
2024-08-08 16:44:27 -0300

    dsgs

--- src/components/pages/AnheloRidersStats.jsx
+++ src/components/pages/AnheloRidersStats.jsx
@@ -55,12 +55,12 @@ export const AnheloRidersStats = () => {
 					<p>
 						Capaz de sacar máximo 4 pedidos por vuelta sin descuidar la entrega.
 					</p>
-					<div className="flex flex-row gap-4 border-2 mt-4 justify-between p-2">
+					<div className="flex flex-row gap-4 border-2 items-center border-green-500 bg-black mt-4 justify-between p-2">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 24 24"
 							fill="currentColor"
-							className="h-16 text-white"
+							className="h-16 text-green-500"
 						>
 							<path d="M21 6.375c0 2.692-4.03 4.875-9 4.875S3 9.067 3 6.375 7.03 1.5 12 1.5s9 2.183 9 4.875Z" />
 							<path d="M12 12.75c2.685 0 5.19-.586 7.078-1.609a8.283 8.283 0 0 0 1.897-1.384c.016.121.025.244.025.368C21 12.817 16.97 15 12 15s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.285 8.285 0 0 0 1.897 1.384C6.809 12.164 9.315 12.75 12 12.75Z" />
@@ -68,7 +68,7 @@ export const AnheloRidersStats = () => {
 							<path d="M12 20.25c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 0 0 1.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 0 0 1.897 1.384C6.809 19.664 9.315 20.25 12 20.25Z" />
 						</svg>
 
-						<p className="mt-2 text-white">
+						<p className="mt-2 text-green-500">
 							En el siguiente nivel se te habilitaran máximo 5 pedidos por
 							vuelta.
 						</p>
