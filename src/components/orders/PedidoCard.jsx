b26555447eb0519cdcbdce1287dd4397ef9b6613
Tobías <tobiasazcurrabi@gmail.com>
2024-08-12 20:17:24 -0300

    gfdsgs

--- src/components/orders/PedidoCard.jsx
+++ src/components/orders/PedidoCard.jsx
@@ -1,118 +1,118 @@
-import PropTypes from 'prop-types';
-import { currencyFormat } from '../../helpers/currencyFormat';
-import { marcarPedidoComoEntregado } from '../../firebase/orders';
-import Swal from 'sweetalert2';
-import { calcularDemora } from '../../helpers/calcularDemora';
+import PropTypes from "prop-types";
+import { currencyFormat } from "../../helpers/currencyFormat";
+import { marcarPedidoComoEntregado } from "../../firebase/orders";
+import Swal from "sweetalert2";
+import { calcularDemora } from "../../helpers/calcularDemora";
 export const PedidoCard = ({
-  direccion,
-  hora,
-  total,
-  id,
-  fecha,
-  telefono,
-  isVisible,
-  entregado,
-  metodoPago,
-  index,
+	direccion,
+	hora,
+	total,
+	id,
+	fecha,
+	telefono,
+	isVisible,
+	entregado,
+	metodoPago,
+	index,
 }) => (
-  <div
-    className={`flex flex-row justify-between border-b border-b-red-main transition-transform duration-300 ease-in-out ${
-      isVisible ? 'transform-none' : 'transform -translate-y-full'
-    }`}
-    style={{ transitionDelay: `${index * 100}ms` }}
-  >
-    <div className="flex flex-col p-4">
-      <p className="uppercase mb-2 mt-2 bg-black font-black text-red-main font-antonio">
-        {direccion}
-      </p>
-      <p className="text-red-main text-xs font-black font-antonio">
-        Demora: {calcularDemora(hora)}
-      </p>
-      <p className="text-red-main text-xs font-black font-antonio">
-        {metodoPago === 'efectivo'
-          ? `Cobrar: ${currencyFormat(total)}`
-          : `Cobrar: PAGADO`}
-      </p>
-    </div>
-    <div className="flex flex-col justify-between">
-      <a
-        href={`tel:${telefono}`}
-        className="
+	<div
+		className={`flex flex-row justify-between bg-gray-200 hover:bg-gray-300 transition-transform duration-300 ease-in-out ${
+			isVisible ? "transform-none" : "transform -translate-y-full"
+		}`}
+		style={{ transitionDelay: `${index * 100}ms` }}
+	>
+		<div className="flex flex-col p-4">
+			<p className="uppercase mb-2 mt-2  font-black text-black font-antonio">
+				{direccion}
+			</p>
+			<p className="text-black text-xs font-black font-antonio">
+				Demora: {calcularDemora(hora)}
+			</p>
+			<p className="text-black text-xs font-black font-antonio">
+				{metodoPago === "efectivo"
+					? `Cobrar: ${currencyFormat(total)}`
+					: `Cobrar: PAGADO`}
+			</p>
+		</div>
+		<div className="flex flex-col justify-between">
+			<a
+				href={`tel:${telefono}`}
+				className="
         cursor-pointer hover:bg-red-400 
         uppercase p-2 flex flex-row items-center h-1/2 bg-white font-black justify-center text-black font-antonio gap-2"
-      >
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className=" h-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
-            />
-          </svg>
+			>
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth="1.5"
+						stroke="currentColor"
+						className=" h-4"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+						/>
+					</svg>
 
-          <p>LLAMAR</p>
-        </div>
-      </a>
+					<p>LLAMAR</p>
+				</div>
+			</a>
 
-      {!entregado && (
-        <div
-          onClick={() =>
-            marcarPedidoComoEntregado(id, fecha)
-              .then(() => {
-                Swal.fire({
-                  icon: 'success',
-                  title: 'ENTREGADOOOOOOOOOOOOOOOO',
-                  text: `El pedido con ID ${id} ha sido entregado.`,
-                });
-              })
-              .catch(() => {
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Error',
-                  text: 'No se pudo entrerga el pedido.',
-                });
-              })
-          }
-          className="cursor-pointer uppercase p-2 flex flex-row items-center h-1/2 bg-green-500 font-black justify-center text-black font-antonio gap-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className=" h-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+			{!entregado && (
+				<div
+					onClick={() =>
+						marcarPedidoComoEntregado(id, fecha)
+							.then(() => {
+								Swal.fire({
+									icon: "success",
+									title: "ENTREGADOOOOOOOOOOOOOOOO",
+									text: `El pedido con ID ${id} ha sido entregado.`,
+								});
+							})
+							.catch(() => {
+								Swal.fire({
+									icon: "error",
+									title: "Error",
+									text: "No se pudo entrerga el pedido.",
+								});
+							})
+					}
+					className="cursor-pointer uppercase p-2 flex flex-row items-center h-1/2 bg-green-500 font-black justify-center text-black font-antonio gap-2"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth="1.5"
+						stroke="currentColor"
+						className=" h-5"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/>
+					</svg>
 
-          <p>ENTREGAR</p>
-        </div>
-      )}
-    </div>
-  </div>
+					<p>ENTREGAR</p>
+				</div>
+			)}
+		</div>
+	</div>
 );
 
 PedidoCard.propTypes = {
-  direccion: PropTypes.string.isRequired,
-  hora: PropTypes.string.isRequired,
-  total: PropTypes.number.isRequired,
-  isVisible: PropTypes.bool,
-  index: PropTypes.number,
-  entregado: PropTypes.bool,
-  fecha: PropTypes.string,
-  id: PropTypes.string,
-  telefono: PropTypes.string,
-  metodoPago: PropTypes.string,
+	direccion: PropTypes.string.isRequired,
+	hora: PropTypes.string.isRequired,
+	total: PropTypes.number.isRequired,
+	isVisible: PropTypes.bool,
+	index: PropTypes.number,
+	entregado: PropTypes.bool,
+	fecha: PropTypes.string,
+	id: PropTypes.string,
+	telefono: PropTypes.string,
+	metodoPago: PropTypes.string,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
-  ],
-  theme: {
-    extend: {
-      backgroundColor: {
-        'custom-red': '#FE0000',
-      },
-      textColor: {
-        'custom-red': '#FE0000',
-      },
-      fontFamily: {
-        oswald: ['Oswald', 'sans-serif'],
-        antonio: ['Antonio', 'sans-serif'],
-        kotch: ['Koch Fette Deutsche Schrift', 'sans-serif'],
-      },
-      screens: {
-        random: '463px',
-      },
-      colors: {
-        'red-main': '#ff0000',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+	],
+	theme: {
+		extend: {
+			backgroundColor: {
+				"custom-red": "#FE0000",
+			},
+			textColor: {
+				"custom-red": "#FE0000",
+			},
+			fontFamily: {
+				coolvetica: ["Coolvetica", "sans-serif"],
+				"coolvetica-compressed": ["Coolvetica Compressed", "sans-serif"],
+				"coolvetica-condensed": ["Coolvetica Condensed", "sans-serif"],
+				"coolvetica-crammed": ["Coolvetica Crammed", "sans-serif"],
+				"coolvetica-ul": ["Coolvetica Ul", "sans-serif"],
+				oswald: ["Oswald", "sans-serif"],
+				antonio: ["Antonio", "sans-serif"],
+				kotch: ["Koch Fette Deutsche Schrift", "sans-serif"],
+			},
+			fontWeight: {
+				thin: "200",
+				light: "300",
+				normal: "400",
+				bold: "700",
+				black: "900",
+			},
+			screens: {
+				random: "463px",
+			},
+			colors: {
+				"red-main": "#ff0000",
+			},
+		},
+	},
+	plugins: [],
 };
