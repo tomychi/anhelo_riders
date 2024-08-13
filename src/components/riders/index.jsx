ea50b371e0192ee702c3c58c10ed285cd6ef8bf8
Tobías <tobiasazcurrabi@gmail.com>
2024-08-13 17:38:22 -0300

    gsdgs

--- src/components/riders/index.jsx
+++ src/components/riders/index.jsx
@@ -1,113 +1,128 @@
-import { pedidoPropTypes } from '../../helpers/propTypes';
-import PropTypes from 'prop-types';
-import { endRide, startRide } from '../../firebase/users';
-import { useDispatch, useSelector } from 'react-redux';
-import { setRideStatus } from '../../redux/riders/riderAction';
-import Swal from 'sweetalert2';
+import { pedidoPropTypes } from "../../helpers/propTypes";
+import PropTypes from "prop-types";
+import { endRide, startRide } from "../../firebase/users";
+import { useDispatch, useSelector } from "react-redux";
+import { setRideStatus } from "../../redux/riders/riderAction";
+import clock from "../../assets/clockIcon.png";
+import Swal from "sweetalert2";
 const RideComponent = ({
-  pedidosPorEntregar,
-  totalDuration,
-  totalDistance,
+	pedidosPorEntregar,
+	totalDuration,
+	totalDistance,
 }) => {
-  const user = useSelector((state) => state.auth.user);
-  const cadeteId = user.uid;
+	const user = useSelector((state) => state.auth.user);
+	const cadeteId = user.uid;
 
-  const dispatch = useDispatch();
-  const { rideId, isAvailable } = useSelector((state) => state.ride);
-  const handleStartRide = async () => {
-    try {
-      if (pedidosPorEntregar.length === 0) {
-        console.error('No hay pedidos por entregar.');
-        return;
-      }
+	const dispatch = useDispatch();
+	const { rideId, isAvailable } = useSelector((state) => state.ride);
+	const handleStartRide = async () => {
+		try {
+			if (pedidosPorEntregar.length === 0) {
+				console.error("No hay pedidos por entregar.");
+				return;
+			}
 
-      const newRideId = await startRide(
-        cadeteId,
-        pedidosPorEntregar,
-        totalDistance,
-        totalDuration
-      );
-      if (newRideId) {
-        dispatch(setRideStatus(newRideId, false, pedidosPorEntregar));
-      } else {
-        console.error('No se pudo iniciar la vuelta.');
-      }
-    } catch (error) {
-      console.error('Error al iniciar la vuelta', error);
-    }
-  };
+			const newRideId = await startRide(
+				cadeteId,
+				pedidosPorEntregar,
+				totalDistance,
+				totalDuration
+			);
+			if (newRideId) {
+				dispatch(setRideStatus(newRideId, false, pedidosPorEntregar));
+			} else {
+				console.error("No se pudo iniciar la vuelta.");
+			}
+		} catch (error) {
+			console.error("Error al iniciar la vuelta", error);
+		}
+	};
 
-  const handleEndRide = async () => {
-    if (!rideId) {
-      console.error('El ID de la vuelta no está definido');
-      return;
-    }
+	const handleEndRide = async () => {
+		if (!rideId) {
+			console.error("El ID de la vuelta no está definido");
+			return;
+		}
 
-    // Verifica si todos los pedidos en vuelta están marcados como 'delivered'
-    const allDelivered = pedidosPorEntregar.every(
-      (order) => order.status === 'delivered'
-    );
+		// Verifica si todos los pedidos en vuelta están marcados como 'delivered'
+		const allDelivered = pedidosPorEntregar.every(
+			(order) => order.status === "delivered"
+		);
 
-    if (!allDelivered) {
-      console.error('No todos los pedidos están marcados como entregados.');
+		if (!allDelivered) {
+			console.error("No todos los pedidos están marcados como entregados.");
 
-      Swal.fire({
-        icon: 'warning',
-        title: 'Pedidos Incompletos',
-        text: 'No todos los pedidos están marcados como entregados. Por favor, verifica los pedidos restantes.',
-        confirmButtonText: 'Entendido',
-        confirmButtonColor: '#3085d6',
-      });
+			Swal.fire({
+				icon: "warning",
+				title: "Pedidos Incompletos",
+				text: "No todos los pedidos están marcados como entregados. Por favor, verifica los pedidos restantes.",
+				confirmButtonText: "Entendido",
+				confirmButtonColor: "#3085d6",
+			});
 
-      return;
-    }
+			return;
+		}
 
-    try {
-      await endRide(rideId, cadeteId);
-      dispatch(setRideStatus(null, true, []));
-    } catch (error) {
-      console.error('Error al finalizar la vuelta', error);
-    }
-  };
-  return (
-    <div
-      className={`absolute bg-black ${
-        (isAvailable && pedidosPorEntregar.length > 0) ||
-        (!isAvailable && pedidosPorEntregar.length === 0)
-          ? 'inset-0 bg-opacity-50'
-          : 'hidden'
-      } flex flex-col items-center justify-center p-4 gap-4`}
-    >
-      {/* Mostrar botón de iniciar vuelta solo si no hay vuelta en curso y hay pedidos por entregar */}
-      {isAvailable && pedidosPorEntregar.length > 0 && (
-        <button
-          onClick={handleStartRide}
-          className="bg-black text-gray-100 w-full max-w-md font-medium py-4"
-        >
-          Confirmar salida
-          <p className="bg-white text-xs text-black font-bold px-4 py-2 leading-none">
-            {totalDuration.toFixed(2)} Min.
-          </p>
-        </button>
-      )}
+		try {
+			await endRide(rideId, cadeteId);
+			dispatch(setRideStatus(null, true, []));
+		} catch (error) {
+			console.error("Error al finalizar la vuelta", error);
+		}
+	};
+	return (
+		<div
+			className={`absolute bg-black ${
+				(isAvailable && pedidosPorEntregar.length > 0) ||
+				(!isAvailable && pedidosPorEntregar.length === 0)
+					? "inset-0 bg-opacity-50"
+					: "hidden"
+			} flex flex-col items-center justify-center p-4 gap-4`}
+		>
+			{/* Mostrar botón de iniciar vuelta solo si no hay vuelta en curso y hay pedidos por entregar */}
+			{isAvailable && pedidosPorEntregar.length > 0 && (
+				<div className="flex flex-row w-full gap-4">
+					<button
+						onClick={handleStartRide}
+						className="bg-black h-14 text-gray-100 text-xl items-center w-full font-medium p-4"
+					>
+						Confirmar salida
+					</button>
+					<div className="h-14 bg-gray-100 relative">
+						<img
+							src={clock}
+							className="h-2 absolute top-0 right-0 m-2"
+							alt=""
+						/>
+						<p className="text-xs text-black lowercase flex items-center text-center justify-center font-bold p-4 leading-none">
+							{Math.floor(totalDuration)} Minutos
+						</p>
+					</div>
+				</div>
+			)}
 
-      {/* Mostrar botón de finalizar vuelta solo si hay una vuelta en curso y todos los pedidos en vuelta están entregados */}
-      {!isAvailable && pedidosPorEntregar.length === 0 && (
-        <button
-          onClick={handleEndRide}
-          className="bg-black text-gray-100 w-full max-w-md font-medium py-4"
-        >
-          Llegue Anhelo
-        </button>
-      )}
-    </div>
-  );
+			{/* Mostrar botón de finalizar vuelta solo si hay una vuelta en curso y todos los pedidos en vuelta están entregados */}
+			{!isAvailable && pedidosPorEntregar.length === 0 && (
+				<div className="flex flex-row w-full gap-4">
+					<button
+						onClick={handleEndRide}
+						className="bg-black text-gray-100 h-14 text-xl items-center w-full font-medium p-4"
+					>
+						Llegue a Anhelo
+					</button>
+					<p className="bg-green-500 text-xl text-black h-14 flex items-center font-bold p-4  leading-none">
+						+$7890
+					</p>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default RideComponent;
 
 RideComponent.propTypes = {
-  pedidosPorEntregar: PropTypes.arrayOf(pedidoPropTypes).isRequired,
-  totalDistance: PropTypes.number,
-  totalDuration: PropTypes.number,
+	pedidosPorEntregar: PropTypes.arrayOf(pedidoPropTypes).isRequired,
+	totalDistance: PropTypes.number,
+	totalDuration: PropTypes.number,
 };
