640608fa5abd962fca3fa8a6e8243de546a7bf75
Tobías <tobiasazcurrabi@gmail.com>
2024-08-12 21:42:27 -0300

    fdafa

--- src/components/map/MapOrders.jsx
+++ src/components/map/MapOrders.jsx
@@ -1,161 +1,148 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  useMap,
-  useMapsLibrary,
-} from '@vis.gl/react-google-maps';
-import PropTypes from 'prop-types';
-import { pedidoPropTypes } from '../../helpers/propTypes';
-import RideComponent from '../riders';
+	APIProvider,
+	Map,
+	AdvancedMarker,
+	useMap,
+	useMapsLibrary,
+} from "@vis.gl/react-google-maps";
+import PropTypes from "prop-types";
+import { pedidoPropTypes } from "../../helpers/propTypes";
+import RideComponent from "../riders";
 
 const APIKEY = import.meta.env.VITE_API_GOOGLE_MAPS;
 
 const position = { lat: -33.117142, lng: -64.347756 };
 const origen = { lat: -33.095809, lng: -64.33412 };
 
-// const origen = { lat: -33.122869792517136, lng: -64.3548615327737 };
-
 export const MapOrders = ({ orders }) => {
-  return (
-    <APIProvider apiKey={APIKEY}>
-      <div className="absolute inset-0 w-full h-full object-cover">
-        <Map
-          defaultZoom={13}
-          defaultCenter={position}
-          mapId={'bf51a910020fa25a'}
-          gestureHandling={'greedy'}
-          disableDefaultUI={true}
-        >
-          {/* {orders.map((order, index) => (
-						<AdvancedMarker
-							key={index}
-							position={{ lat: order.map[0], lng: order.map[1] }}
-							draggable={false}
-						/>
-					))} */}
-          <Directions orders={orders} />
-        </Map>
-      </div>
-    </APIProvider>
-  );
+	const [vueltaIniciada, setVueltaIniciada] = useState(false);
+
+	return (
+		<APIProvider apiKey={APIKEY}>
+			<div className="relative w-full h-full">
+				<Map
+					defaultZoom={13}
+					defaultCenter={position}
+					mapId={"bf51a910020fa25a"}
+					gestureHandling={"greedy"}
+					disableDefaultUI={true}
+				>
+					<Directions orders={orders} />
+				</Map>
+
+				{!vueltaIniciada && (
+					<div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+						<button
+							onClick={() => setVueltaIniciada(true)}
+							className="bg-white text-black font-bold py-2 px-4 rounded"
+						>
+							Iniciar Vuelta
+						</button>
+					</div>
+				)}
+			</div>
+		</APIProvider>
+	);
 };
 
 function Directions({ orders }) {
-  const map = useMap();
-  const routesLibrary = useMapsLibrary('routes');
+	const map = useMap();
+	const routesLibrary = useMapsLibrary("routes");
 
-  const [directionsService, setDirectionsService] = useState();
-  const [directionsRenderer, setDirectionsRenderer] = useState();
-  const [totalDistance, setTotalDistance] = useState(0);
-  const [totalDuration, setTotalDuration] = useState(0);
+	const [directionsService, setDirectionsService] = useState();
+	const [directionsRenderer, setDirectionsRenderer] = useState();
+	const [totalDistance, setTotalDistance] = useState(0);
+	const [totalDuration, setTotalDuration] = useState(0);
 
-  const [routes, setRoutes] = useState([]);
-  const [routeIndex, setRouteIndex] = useState(0);
-  const selected = routes[routeIndex];
-  const leg = selected?.legs[0];
+	const [routes, setRoutes] = useState([]);
+	const [routeIndex, setRouteIndex] = useState(0);
+	const selected = routes[routeIndex];
+	const leg = selected?.legs[0];
 
-  useEffect(() => {
-    if (!routesLibrary || !map) return;
+	useEffect(() => {
+		if (!routesLibrary || !map) return;
 
-    setDirectionsService(new routesLibrary.DirectionsService());
-    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
-  }, [routesLibrary, map]);
+		setDirectionsService(new routesLibrary.DirectionsService());
+		setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+	}, [routesLibrary, map]);
 
-  useEffect(() => {
-    if (!directionsService || !directionsRenderer) return;
+	useEffect(() => {
+		if (!directionsService || !directionsRenderer) return;
 
-    const waypoints = orders.map((order) => ({
-      location: { lat: order.map[0], lng: order.map[1] },
-      stopover: true,
-    }));
+		const waypoints = orders.map((order) => ({
+			location: { lat: order.map[0], lng: order.map[1] },
+			stopover: true,
+		}));
 
-    directionsService
-      .route({
-        origin: origen,
-        destination: origen,
-        waypoints: waypoints,
-        optimizeWaypoints: true,
-        travelMode: window.google.maps.TravelMode.DRIVING,
-        provideRouteAlternatives: true,
-      })
-      .then((response) => {
-        directionsRenderer.setDirections(response);
+		directionsService
+			.route({
+				origin: origen,
+				destination: origen,
+				waypoints: waypoints,
+				optimizeWaypoints: true,
+				travelMode: window.google.maps.TravelMode.DRIVING,
+				provideRouteAlternatives: true,
+			})
+			.then((response) => {
+				directionsRenderer.setDirections(response);
 
-        // Calcular la distancia total
-        const route = response.routes[0];
-        const totalDistance = route.legs.reduce((acc, leg) => {
-          return acc + leg.distance.value; // Sumar las distancias en metros
-        }, 0);
-        const totalDuration = route.legs.reduce(
-          (acc, leg) => acc + leg.duration.value,
-          0
-        );
+				const route = response.routes[0];
+				const totalDistance = route.legs.reduce((acc, leg) => {
+					return acc + leg.distance.value;
+				}, 0);
+				const totalDuration = route.legs.reduce(
+					(acc, leg) => acc + leg.duration.value,
+					0
+				);
 
-        setTotalDistance(totalDistance / 1000);
-        setTotalDuration(totalDuration / 60); // Convertir a minutos
+				setTotalDistance(totalDistance / 1000);
+				setTotalDuration(totalDuration / 60);
 
-        const lineSymbol = {
-          path: window.google.maps.SymbolPath.FORWARD_CLOSED_ARROW, // Define una flecha cerrada como símbolo
-          scale: 2, // Escala del símbolo
-          strokeColor: '#FF0000', // Color de la flecha
-        };
+				const lineSymbol = {
+					path: window.google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+					scale: 2,
+					strokeColor: "#FF0000",
+				};
 
-        // Configurar la Polyline con flechas
-        directionsRenderer.setOptions({
-          polylineOptions: {
-            strokeColor: '#00AAFF',
-            strokeOpacity: 0.7,
-            strokeWeight: 4,
-            icons: [
-              {
-                icon: lineSymbol,
-                offset: '100%', // Desplazamiento de las flechas
-                repeat: '20px', // Espaciado entre las flechas
-              },
-            ],
-          },
-        });
+				directionsRenderer.setOptions({
+					polylineOptions: {
+						strokeColor: "#00AAFF",
+						strokeOpacity: 0.7,
+						strokeWeight: 4,
+						icons: [
+							{
+								icon: lineSymbol,
+								offset: "100%",
+								repeat: "20px",
+							},
+						],
+					},
+				});
 
-        setRoutes(response.routes);
-      });
-  }, [directionsService, directionsRenderer, orders]);
-  if (!leg) return null;
+				setRoutes(response.routes);
+			});
+	}, [directionsService, directionsRenderer, orders]);
 
-  return (
-    <div className="directions">
-      <RideComponent
-        pedidosPorEntregar={orders}
-        totalDistance={totalDistance}
-        totalDuration={totalDuration}
-      />
-      {/* <h2>{selected.summary}</h2> */}
-      <p>
-        {/* {leg.start_address.split(',')[0]} to {leg.end_address.split(',')[0]} */}
-      </p>
-      <p>Distancia total: {totalDistance.toFixed(2)} km</p>
-      <p>Duración total: {totalDuration.toFixed(2)} minutos</p>
+	if (!leg) return null;
 
-      {/* 
-      <h2>Other Routes</h2>
-      <ul>
-        {routes.map((route, index) => (
-          <li key={route.summary}>
-            <button onClick={() => setRouteIndex(index)}>
-              {route.summary}
-            </button>
-          </li>
-        ))}
-      </ul> */}
-    </div>
-  );
+	return (
+		<div className="directions">
+			<RideComponent
+				pedidosPorEntregar={orders}
+				totalDistance={totalDistance}
+				totalDuration={totalDuration}
+			/>
+			<p>Distancia total: {totalDistance.toFixed(2)} km</p>
+			<p>Duración total: {totalDuration.toFixed(2)} minutos</p>
+		</div>
+	);
 }
 
 MapOrders.propTypes = {
-  orders: PropTypes.arrayOf(pedidoPropTypes).isRequired,
+	orders: PropTypes.arrayOf(pedidoPropTypes).isRequired,
 };
 
 Directions.propTypes = {
-  orders: PropTypes.arrayOf(pedidoPropTypes).isRequired,
+	orders: PropTypes.arrayOf(pedidoPropTypes).isRequired,
 };
