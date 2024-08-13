fb0deda444598928c78621c92e919d5bd9e37eb2
Tobías <tobiasazcurrabi@gmail.com>
2024-08-13 13:14:49 -0300

    sfa

--- src/components/pages/AnheloRiders.jsx
+++ src/components/pages/AnheloRiders.jsx
@@ -109,18 +109,16 @@ export const AnheloRiders = () => {
 				<NavLink to="/anheloriders_stats" className="absolute right-4">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						className={`h-6 w-6 transition-transform duration-300 ${
-							visibleSection === "porEntregar" ? "rotate-180" : ""
-						}`}
 						fill="none"
 						viewBox="0 0 24 24"
+						strokeWidth="1.5"
 						stroke="currentColor"
+						className="h-3 text-black"
 					>
 						<path
 							strokeLinecap="round"
 							strokeLinejoin="round"
-							strokeWidth={2}
-							d="M19 9l-7 7-7-7"
+							d="M8.25 4.5l7.5 7.5-7.5 7.5"
 						/>
 					</svg>
 				</NavLink>
