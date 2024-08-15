9bffc33080a97a3cd1e59d9b74791996daaea04c
Tobías <tobiasazcurrabi@gmail.com>
2024-08-15 13:32:19 -0300

    gdsaga

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AnheloRiders } from '../components/pages/AnheloRiders';
-import { AnheloRidersStats } from '../components/pages/AnheloRidersStats';
-import { Login } from '../components/pages/LogIg';
-import { PrivateRoutesLayout } from '../layouts/PrivateRoutesLayout';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AnheloRiders } from "../components/pages/AnheloRiders";
+import { AnheloRidersStats } from "../components/pages/AnheloRidersStats";
+import { Login } from "../components/pages/LogIg";
+import { PrivateRoutesLayout } from "../layouts/PrivateRoutesLayout";
 
 export const Navigation = () => {
-  return (
-    <Router>
-      <div className="h-screen  overflow-x-hidden">
-        <Routes>
-          {/* Por default vas aca */}
-          <Route path="/login" element={<Login />} />
+	return (
+		<Router>
+			<div className="h-screen bg-gray-100  overflow-x-hidden">
+				<Routes>
+					{/* Por default vas aca */}
+					<Route path="/login" element={<Login />} />
 
-          {/* Estas rutas tengo que hacerlas privadas */}
+					{/* Estas rutas tengo que hacerlas privadas */}
 
-          {/* Rutas para el dashboard */}
-          <Route element={<PrivateRoutesLayout />}>
-            <Route path="/*" element={<AnheloRiders />} />
-            <Route path="/anheloriders" element={<AnheloRiders />} />
-            <Route path="anheloriders_stats" element={<AnheloRidersStats />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
-  );
+					{/* Rutas para el dashboard */}
+					<Route element={<PrivateRoutesLayout />}>
+						<Route path="/*" element={<AnheloRiders />} />
+						<Route path="/anheloriders" element={<AnheloRiders />} />
+						<Route path="anheloriders_stats" element={<AnheloRidersStats />} />
+					</Route>
+				</Routes>
+			</div>
+		</Router>
+	);
 };
