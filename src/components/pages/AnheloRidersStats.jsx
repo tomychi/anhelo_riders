04934626236a276d41388b2ec08c13996dbd99d8
Tobías <tobiasazcurrabi@gmail.com>
2024-08-15 14:46:07 -0300

    gdsgs

--- src/components/pages/AnheloRidersStats.jsx
+++ src/components/pages/AnheloRidersStats.jsx
@@ -316,8 +316,23 @@ export const AnheloRidersStats = () => {
 								</div>
 							</div>
 						</div>
-						<div className="bg-red-main text-white text-center py-3 font-medium cursor-pointer">
-							Ver mas
+						<div className="bg-red-main flex flex-row items-center gap-1 justify-center text-white text-center py-3  cursor-pointer">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className="h-4"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+								/>
+							</svg>
+
+							<p className="text-xl fond-medium items-center">Ver mas</p>
 						</div>
 					</div>
 				</div>
