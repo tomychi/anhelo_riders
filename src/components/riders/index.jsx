cae34fd86fe835b961b917a6badb2d933e55e991
tomy <tomychi352@gmail.com>
2024-09-06 10:19:41 -0300

    rider anda

--- src/components/riders/index.jsx
+++ src/components/riders/index.jsx
@@ -161,63 +161,67 @@ const RideComponent = ({
 
   if (!isAvailable) {
     return (
-      <div className="fixed inset-0 flex items-center justify-center z-50">
+      <>
         <button
           onClick={handleCancelRide}
-          className="fixed bottom-4 right-4 bg-red-500 h-12 w-12 text-gray-100 hover:text-white uppercase text-xl rounded-full flex items-center justify-center font-medium p-2"
+          className="fixed bottom-4 right-4 bg-red-500 h-12 w-12 text-gray-100 hover:text-white uppercase text-xl rounded-full flex items-center justify-center font-medium p-2 z-50"
         >
           X
         </button>
         {pedidosPorEntregar.length === 0 && (
-          <div className="bg-gray-100 text-black rounded-md shadow-lg max-w-sm w-full mx-4">
-            <div className="px-4 py-6">
-              <div className="flex flex-col items-center text-center">
-                <p className="text-xl font-bold mb-2">Vuelta casi terminada</p>
-                <p className="text-sm font-medium leading-5 mb-4">
-                  Cuando regreses a Anhelo podrás apretar el siguiente botón y
-                  se te acreditará {currencyFormat(paga)}.
-                </p>
+          <div className="fixed inset-0 flex items-center justify-center z-40">
+            <div className="bg-gray-100 text-black rounded-md shadow-lg max-w-sm w-full mx-4">
+              <div className="px-4 py-6">
+                <div className="flex flex-col items-center text-center">
+                  <p className="text-xl font-bold mb-2">
+                    Vuelta casi terminada
+                  </p>
+                  <p className="text-sm font-medium leading-5 mb-4">
+                    Cuando regreses a Anhelo podrás apretar el siguiente botón y
+                    se te acreditará {currencyFormat(paga)}.
+                  </p>
+                </div>
               </div>
+              <button
+                onClick={handleEndRide}
+                className="w-full border-t border-black rounded-b-md text-xl bg-black text-gray-100 text-center py-4 font-medium cursor-pointer hover:bg-gray-900 transition-colors"
+              >
+                Llegue a Anhelo
+              </button>
             </div>
-            <button
-              onClick={handleEndRide}
-              className="w-full border-t border-black rounded-b-md text-xl bg-black text-gray-100 text-center py-4 font-medium cursor-pointer hover:bg-gray-900 transition-colors"
-            >
-              Llegue a Anhelo
-            </button>
           </div>
         )}
-      </div>
+      </>
     );
   }
 
   return (
     <div
-      className={`absolute bg-black ${
+      className={`fixed inset-0 bg-black ${
         isAvailable && pedidosPorEntregar.length > 0
-          ? 'inset-0 bg-opacity-50'
+          ? 'bg-opacity-50'
           : 'hidden'
-      } flex flex-col items-center justify-center p-4 gap-4`}
+      } flex flex-col items-center justify-center p-4 gap-4 z-30`}
     >
       {/* Mostrar botón de iniciar vuelta solo si no hay vuelta en curso y hay pedidos por entregar */}
       {isAvailable && pedidosPorEntregar.length > 0 && (
-        <div className="flex flex-col w-full items-center gap-2">
+        <div className="flex flex-col w-full items-center gap-2 max-w-sm">
           <button
             onClick={handleStartRide}
-            className="bg-black h-14 text-gray-100 hover:text-green-500  uppercase text-xl items-center w-full rounded-md font-medium p-4"
+            className="bg-black h-14 text-gray-100 hover:text-green-500 uppercase text-xl items-center w-full rounded-md font-medium p-4"
           >
             COMENZAR VUELTA
           </button>
 
-          <div className="flex flex-row w-full gap-2 justify-center ">
-            <div className="h-5 rounded-md flex flex-row items-center bg-gray-100 w-1/2 p-4 gap-2 text-center justify-center ">
-              <img src={clock} className="h-2 " alt="" />
-              <p className="text-xs text-black lowercase mb-[-4px]  flex  text-center justify-center font-medium">
+          <div className="flex flex-row w-full gap-2 justify-center">
+            <div className="h-5 rounded-md flex flex-row items-center bg-gray-100 w-1/2 p-4 gap-2 text-center justify-center">
+              <img src={clock} className="h-2" alt="" />
+              <p className="text-xs text-black lowercase mb-[-4px] flex text-center justify-center font-medium">
                 {Math.floor(totalDuration)} Minutos aprox.
               </p>
             </div>
-            <div className="h-5 rounded-md flex flex-row items-center bg-gray-100 w-1/2 p-4 gap-2 text-center justify-center ">
-              <p className="text-xs text-black lowercase mb-[-4px] flex  text-center justify-center font-medium  leading-none">
+            <div className="h-5 rounded-md flex flex-row items-center bg-gray-100 w-1/2 p-4 gap-2 text-center justify-center">
+              <p className="text-xs text-black lowercase mb-[-4px] flex text-center justify-center font-medium leading-none">
                 {currencyFormat(paga)} de ganancia
               </p>
             </div>
@@ -227,7 +231,6 @@ const RideComponent = ({
     </div>
   );
 };
-
 export default RideComponent;
 
 RideComponent.propTypes = {
