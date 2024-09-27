323525c8c24192238a12735b519fbbe0c9dad927
tomy <tomychi352@gmail.com>
2024-09-27 16:51:16 -0300

    no repetir vueltas

--- src/firebase/users.js
+++ src/firebase/users.js
@@ -90,16 +90,41 @@ export const startRide = async (
 ) => {
   const startTime = new Date();
   const firestore = getFirestore();
-  const fechaActual = obtenerFechaActual();
-  const [dia, mes, anio] = fechaActual.split("/");
 
+  // Genera un ID único usando timestamp
   const rideId = new Date().getTime().toString();
-  // Actualiza el documento del usuario para iniciar una nueva vuelta
   const userRef = doc(firestore, "empleados", cadeteId);
+
+  // Obtiene el documento del cadete
+  const userSnapshot = await getDoc(userRef);
+
+  // Si el documento existe
+  if (userSnapshot.exists()) {
+    const cadeteData = userSnapshot.data();
+
+    // Revisa si alguna vuelta tiene los mismos pedidos (misma cantidad y mismos IDs de pedidos)
+    const sameOrdersExist = cadeteData.vueltas?.some((vuelta) => {
+      const existingOrderIds = vuelta.orders
+        .map((order) => order.orderId)
+        .sort();
+      const newOrderIds = orders.map((order) => order.id).sort();
+
+      // Compara los pedidos existentes con los nuevos pedidos
+      return JSON.stringify(existingOrderIds) === JSON.stringify(newOrderIds);
+    });
+
+    // Si ya existe una vuelta con los mismos pedidos, no permite continuar
+    if (sameOrdersExist) {
+      console.log("Ya existe una vuelta con los mismos pedidos.");
+      return null; // O lanza un error o devuelve un valor indicando que no se realizó la operación
+    }
+  }
+
+  // Si no hay vueltas con los mismos pedidos, se crea una nueva vuelta
   await updateDoc(userRef, {
     available: false,
     vueltas: arrayUnion({
-      rideId, // Genera un ID único usando timestamp
+      rideId,
       startTime: startTime,
       orders: orders.map((order) => ({
         orderId: order.id,
@@ -113,27 +138,6 @@ export const startRide = async (
     }),
   });
 
-  const pedidosRef = doc(firestore, "pedidos", anio, mes, dia);
-  await runTransaction(firestore, async (transaction) => {
-    const pedidosDoc = await transaction.get(pedidosRef);
-
-    if (!pedidosDoc.exists()) {
-      transaction.set(pedidosRef, {
-        pedidos: orders.map((order) => ({
-          ...order,
-          status: "pending",
-        })),
-      });
-    } else {
-      const existingPedidos = pedidosDoc.data()?.pedidos || [];
-      const updatedPedidos = existingPedidos.map((pedido) =>
-        orders.some((order) => order.id === pedido.id)
-          ? { ...pedido, status: "pending" }
-          : pedido,
-      );
-      transaction.update(pedidosRef, { pedidos: updatedPedidos });
-    }
-  });
   return rideId;
 };
 
